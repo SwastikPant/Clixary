b13f8ae6b072c4106992296c12345dc80b385cd2
SwastikPant <swastik_p@ee.iitr.ac.in>
2025-12-09 23:29:06 +0530

    Added the events model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+	name = models.CharField(max_length=100)
+	start_date = models.DateTimeField()
+	end_date = models.DateTimeField()
+	description = models.TextField(null=True, blank=True)
+	cover_photo = models.ImageField(upload_to="event_covers/", blank=True, null=True)
+	created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+	is_public = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
